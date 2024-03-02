9b2554fb9360f8f3454a17dd6c940837a7e8efbc
Abhinay92 <abhinay.adiraju@gmail.com>
2024-03-02 14:11:00 +0530

    second commit

---
+++ src/main/java/com/mf/training/devsecops/mfvulnerabileapp/MfvulnerabileappApplication.java
@@ -0,0 +1,13 @@
+package com.mf.training.devsecops.mfvulnerabileapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfvulnerabileappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfvulnerabileappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mf/training/devsecops/mfvulnerabileapp/MfvulnerabileappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mf.training.devsecops.mfvulnerabileapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfvulnerabileappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
